b65d1fcdb3002bfb4de19f978169d5ca1ce7c6d5
m.rozenbeek <m.rozenbeek@CR2021-013>
2025-11-12 13:51:36 +0100

    Add project files.

---
+++ commandline/FileAppendCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.CommandLine;
+
+namespace commandline
+{
+    internal class FileAppendCommand: Command
+    {
+
+        public FileAppendCommand(ReadOnlySpan<Option> options): base("append", "Appends text to a file")
+        {
+            foreach (var option in options)
+            {
+                Add(option);
+            }
+            Add(new Option<string>("--text-to-append")
+            {
+                Description = "The text to append to the file.",
+                Required = true,
+                CustomParser = result => 
+                {
+                    if (string.IsNullOrWhiteSpace(result.Tokens.First().Value))
+                    {
+                        result.AddError("The --text-to-append option requires a non-empty value.");
+                        return string.Empty;
+                    }
+
+                    return result.Tokens.First().Value;
+                }
+            });
+
+            SetAction(parseResult => FileMethods.ReadFileAndAppend(
+                parseResult.GetValue<FileInfo>("--file-path"),
+                parseResult.GetValue<int>("--delay"),
+                parseResult.GetValue<ConsoleColor>("--fgcolor"),
+                parseResult.GetValue<bool>("--light-mode"),
+                parseResult.GetValue<string>("--text-to-append")));
+        }
+    }
+}
---
+++ commandline/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using commandline;
+using System.CommandLine;
+
+RootCommand rootCommand = new("Sample app for System.CommandLine");
+rootCommand.Subcommands.Add(new FileCommand());
+
+
+ParseResult parseResult = rootCommand.Parse(args);
+return parseResult.Invoke();
